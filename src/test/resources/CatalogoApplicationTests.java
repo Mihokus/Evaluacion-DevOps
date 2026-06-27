a8881d43d611472b29d1ae265a8b84b22721b7c4
Mihokus <seba.rodriguezg@duocuc.cl>
2026-06-27 18:51:27 -0400

    elimine carpeta duplicada

---
+++ src/test/resources/CatalogoApplicationTests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
+class CatalogoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
